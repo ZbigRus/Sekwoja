c07c1f492b452fa225617a87308d79459eff146b
Filip <filfer05@gmail.com>
2024-01-24 20:28:53 +0100

    furniture page

--- src/app/meble/page.tsx
+++ src/app/meble/page.tsx
@@ -1,3 +1,13 @@
+import Partners from "@/components/home/partners";
+import Products from "@/components/home/products";
+import Contact from "../kontakt/page";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col">
+      <Products />
+      <Partners />
+      <Contact />
+    </div>
+  );
 }

e32a70f4d344e86f1766eec546c39e2adda1e3a6
Filip <filfer05@gmail.com>
2024-01-26 14:07:18 +0100

    blog

--- src/lib/blog/actions.ts
+++ src/lib/blog/actions.ts
@@ -25,7 +25,6 @@ export async function getPosts(limit?: number) {
         query
       )}`
     );
-    console.log(response);
     const json = await response.json();
     return { data: json.data };
   } catch (err) {

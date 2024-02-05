5435af8275f264abe6855a61e8ff6418b8fbc90e
Filip <filfer05@gmail.com>
2024-02-06 00:01:16 +0100

    cms transfer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "spurious-passivatio.000webhostapp.com" }],
+    remotePatterns: [{ hostname: "api.sekwoja.com" }],
     unoptimized: true,
   },
 };
